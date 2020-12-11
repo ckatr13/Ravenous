4d28980cbb7e0a25914c6408442c661272b514d0
ckatr13 <53585660+ckatr13@users.noreply.github.com>
2020-12-11 15:07:42 -0500

    Updates

---
+++ precache-manifest.6f4a8791508a8becaac0f36eb5fc7d8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff1210a7f59d87c49c4aac63a0b97ac",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "001e58c9c8ac38868452",
+    "url": "/Ravenous/static/css/main.068b38b7.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/Ravenous/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "001e58c9c8ac38868452",
+    "url": "/Ravenous/static/js/main.39b8d307.chunk.js"
+  },
+  {
+    "revision": "5ddfa0b767d8254e6289",
+    "url": "/Ravenous/static/js/runtime~main.9078551a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
